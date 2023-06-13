9aab57a9918b7ff33c31b7fc94be6126be431b9e
ns nasim <subhoislam624@gmail.com>
2023-06-13 13:52:02 +0600

    add edit

--- components/Layout.js
+++ components/Layout.js
@@ -297,7 +297,7 @@ export default function Layout({
 			</Container>
 			{/* socal */}
 			<Script type='text/javascript' src='//pl19713846.highrevenuegate.com/26/f9/50/26f950761cf21432e5082e438a43b6ea.js'></Script>
-
+			<Script type='text/javascript' src='//pl19706691.highrevenuegate.com/37/89/0a/37890a5064b6650b9adfff77672a0fc4.js'></Script>
 			<Footer />
 		</>
 	);
