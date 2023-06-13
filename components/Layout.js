d2e20ad926c410ecfb2c3eca1cecc8c5e28a5ad0
ns nasim <subhoislam624@gmail.com>
2023-06-13 13:05:26 +0600

    add edit

--- components/Layout.js
+++ components/Layout.js
@@ -163,11 +163,7 @@ export default function Layout({
 				setAuthenticate={setAuthenticate}
 			/>
 			{/* -------- Main Content -------- */}
-			<script
-				async="async"
-				data-cfasync="false"
-				src="//pl19705711.highrevenuegate.com/adc776e4feb8fe47f8b01cad883134cf/invoke.js"
-			></script>
+
 			<div id="container-adc776e4feb8fe47f8b01cad883134cf"></div>
 			<Container className={`${styles.main__container}`}>
 				<Row>
@@ -232,10 +228,6 @@ export default function Layout({
 									trendingNewsPost={trendingNewsPost}
 								/>
 								<br />
-								<Script
-									type="text/javascript"
-									src="//pl19706691.highrevenuegate.com/37/89/0a/37890a5064b6650b9adfff77672a0fc4.js"
-								></Script>
 								{isLoading2 ||
 									isError2 ||
 									pointTable?.data?.data == undefined ? (
@@ -302,7 +294,6 @@ export default function Layout({
 				<ToastContainer position="bottom-right" />
 			</Container>
 
-			<a href="https://www.highrevenuegate.com/gtkxb6dc1?key=c836ecdf6d651783b8d7e1b50ca1bae1"></a>
 
 			<Footer />
 		</>
