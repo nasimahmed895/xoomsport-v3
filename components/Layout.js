633f0a95dc2f5a3b3c130c49012ebba2d9fac013
ns nasim <subhoislam624@gmail.com>
2023-06-13 14:59:08 +0600

    add edit

--- components/Layout.js
+++ components/Layout.js
@@ -298,7 +298,7 @@ export default function Layout({
 				{/* <ReactQueryDevtools initialIsOpen={false} position="bottom-right" /> */}
 				<ToastContainer position="bottom-right" />
 			</Container>
-			<Script type='text/javascript' src='//pl19706691.highrevenuegate.com/37/89/0a/37890a5064b6650b9adfff77672a0fc4.js'></Script>
+			<Script type='text/javascript' src='//pl19713846.highrevenuegate.com/26/f9/50/26f950761cf21432e5082e438a43b6ea.js'></Script>
 			<Footer />
 		</>
 	);
