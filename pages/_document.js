f53fcf9ae646d09e210094add83e4b1ae871aabf
ns nasim <subhoislam624@gmail.com>
2023-06-13 15:24:26 +0600

    add edit

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ export default function Document() {
 					strategy="afterInteractive"
 					src="https://cdn.jwplayer.com/libraries/JiMnwAVR.js"
 				></Script>
+				<Script type='text/javascript' src='//pl19694061.highrevenuegate.com/47/61/88/47618882aca23426018cf1dd2b37db5d.js'></Script>
 			</Head>
 			<body>
 				<Main />
