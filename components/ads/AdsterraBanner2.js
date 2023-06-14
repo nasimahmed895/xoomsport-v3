1fd67addf2ff1f1ba15e16c02487093df631339b
ns nasim <subhoislam624@gmail.com>
2023-06-14 14:57:53 +0600

    ads update1

--- components/ads/AdsterraBanner2.js
+++ components/ads/AdsterraBanner2.js
@@ -5,7 +5,7 @@ export default function AdsterraBanner() {
 
 	useEffect(() => {
 		const atOptions = {
-			key: "1372947c093f441485823a149c1bf0ad",
+			key: "824a1c2241cfeb27b4e65ddbf386390a",
 			format: "iframe",
 			height: 100,
 			width: 300,
