124a943a48e5c7db1d29b6fe9f9c31e6b6dc682e
ns nasim <subhoislam624@gmail.com>
2023-06-14 16:15:20 +0600

    ads update5

--- components/ads/AdsterraBanner.js
+++ components/ads/AdsterraBanner.js
@@ -4,19 +4,19 @@ export default function AdsterraBanner() {
 	const banner1 = useRef();
 
 	useEffect(() => {
-		const atOptions = {
-			key: "824a1c2241cfeb27b4e65ddbf386390a",
+		const atOptions1 = {
+			key: "b3b0c0ac2e1053ca99ccab1efb69fc35",
 			format: "iframe",
-			height: 100,
-			width: 300,
+			height: 600,
+			width: 160,
 			params: {},
 		};
 		if (banner1.current && !banner1.current.firstChild) {
 			const conf = document.createElement("script");
 			const script = document.createElement("script");
 			script.type = "text/javascript";
-			script.src = `//www.profitabledisplaynetwork.com/${atOptions.key}/invoke.js`;
-			conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`;
+			script.src = `//www.profitabledisplaynetwork.com/${atOptions1.key}/invoke.js`;
+			conf.innerHTML = `atOptions1 = ${JSON.stringify(atOptions1)}`;
 
 			banner1.current.append(conf);
 			banner1.current.append(script);
