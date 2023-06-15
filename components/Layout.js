74ade120b7bf8b8d87aad4875da1c1dc134fc655
ns nasim <subhoislam624@gmail.com>
2023-06-15 12:14:15 +0600

    ads check5

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,6 @@ import { useQuery } from "react-query";
 // import { ReactQueryDevtoolds } from "react-query/devtools";
 import Script from "next/script";
 import { ToastContainer, toast } from "react-toastify";
-import AdsterraBanner from "./AdsterraBanner";
 import AdsterraBanner2 from "./AdsterraBanner2";
 import ChangePasswordModalForm from "./auth/ChangePasswordModalForm";
 import ForgetPasswordModalForm from "./auth/ForgetPasswordModalForm";
@@ -205,7 +204,7 @@ export default function Layout({
 									</>
 								)}
 								<p>AdsterraBanner </p>
-								<AdsterraBanner />
+								{/* <AdsterraBanner /> */}
 								{isLoading || isError || allLeagues?.data?.data == undefined ? (
 									<LeagueListShimmer />
 								) : (
@@ -238,7 +237,6 @@ export default function Layout({
 								<br />
 								<p>AdsterraBanner2</p>
 								<AdsterraBanner2 />
-
 								{isLoading2 ||
 									isError2 ||
 									pointTable?.data?.data == undefined ? (
