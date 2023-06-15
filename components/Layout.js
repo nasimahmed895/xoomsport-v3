6e908f1de9907880393580b69d9e73922a9961f0
ns nasim <subhoislam624@gmail.com>
2023-06-15 12:16:46 +0600

    ads check6

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,6 @@ import { useQuery } from "react-query";
 // import { ReactQueryDevtoolds } from "react-query/devtools";
 import Script from "next/script";
 import { ToastContainer, toast } from "react-toastify";
-import AdsterraBanner2 from "./AdsterraBanner2";
 import ChangePasswordModalForm from "./auth/ChangePasswordModalForm";
 import ForgetPasswordModalForm from "./auth/ForgetPasswordModalForm";
 import OTPModalForm from "./auth/OTPModalForm";
@@ -203,8 +202,7 @@ export default function Layout({
 										<br />
 									</>
 								)}
-								<p>AdsterraBanner </p>
-								{/* <AdsterraBanner /> */}
+
 								{isLoading || isError || allLeagues?.data?.data == undefined ? (
 									<LeagueListShimmer />
 								) : (
@@ -235,8 +233,7 @@ export default function Layout({
 									trendingNewsPost={trendingNewsPost}
 								/>
 								<br />
-								<p>AdsterraBanner2</p>
-								<AdsterraBanner2 />
+
 								{isLoading2 ||
 									isError2 ||
 									pointTable?.data?.data == undefined ? (
@@ -308,7 +305,6 @@ export default function Layout({
 				type="text/javascript"
 				src="//pl19706691.highrevenuegate.com/37/89/0a/37890a5064b6650b9adfff77672a0fc4.js"
 			></Script>
-			<AdsterraBanner2 />
 			<Footer />
 		</>
 	);
